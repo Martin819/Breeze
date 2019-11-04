9fbea5c5b3fd0501fe8dc9b36db32dac5c6a6c51
Martin Polreich <polreichmartin@gmail.com>
2019-11-04 19:13:37 +0100

    update scripts, add gitignore

--- scripts/ncp_joiner.sh
+++ scripts/ncp_joiner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd openthread
+cd ./openthread
 make -f examples/Makefile-nrf52840 clean
 make -f examples/Makefile-nrf52840 JOINER=1 USB=1
-cd output/nrf52840/bin
+cd ./output/nrf52840/bin
 arm-none-eabi-objcopy -O ihex ot-ncp-ftd ot-ncp-ftd.hex
 echo "----------------------"
 echo ""
@@ -16,6 +16,6 @@ echo "----------------------"
 echo ""
 echo "MANUAL STEP REQUIRED:"
 echo ""
-echo "Run './nrfjprog -f nrf52 -s 683704924 --chiperase --program ~/DP/openthread/output/nrf52840/bin/ot-ncp-ftd.hex --reset' to flash the NS DK board or 'nrfutil pkg generate --hw-version 52 --sd-req 0x00 --application-version 1 --application ~/DP/openthread/output/nrf52840/bin/ot-ncp-ftd.hex ~/DP/openthread/output/nrf52840/bin/ot-ncp-ftd.zip && sudo nrfutil dfu usb-serial -pkg ~/DP/openthread/output/nrf52840/bin/ot-ncp-ftd.zip -p <device>' to flash the usb dongle'"
+echo "Run './nrfjprog -f nrf52 -s 683704924 --chiperase --program ./openthread/output/nrf52840/bin/ot-ncp-ftd.hex --reset' to flash the NS DK board or 'nrfutil pkg generate --hw-version 52 --sd-req 0x00 --application-version 1 --application ./openthread/output/nrf52840/bin/ot-ncp-ftd.hex ./openthread/output/nrf52840/bin/ot-ncp-ftd.zip && sudo nrfutil dfu usb-serial -pkg ./openthread/output/nrf52840/bin/ot-ncp-ftd.zip -p <device>' to flash the usb dongle'"
 echo ""
 echo "----------------------"
